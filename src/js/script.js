499422166c481510dcf519c0da87adce7ae3a90c
Schislyaev <80824032+Schislyaev@users.noreply.github.com>
2022-06-20 15:00:24 +0300

    add Animation

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,61 @@ $(document).ready(function(){
           });
       });
 
+	function validateForm(form) {
+		$(form).validate({
+			rules: {
+				name: "required",
+				phone: "required",
+				email: {
+				required: true,
+				email: true
+				}
+			},
+			messages: {
+				name: "Please give your name",
+				phone: "Give your phone",
+				email: {
+					required: "We need you email",
+					email: "Give the right email"
+				}
+			}
+		});
+	}
+
+	validateForm('#consultation form');
+	validateForm('#consultation-form');
+	validateForm('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-9999");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if(!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function(){
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	})
+
+	//Smooth scroll and page up
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
 });
 
 
